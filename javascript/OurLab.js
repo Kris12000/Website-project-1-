a7dbee8cf3d5ae30a6bc2d6c75349c0027f05ce9
Kris12000 <156031275+Kris12000@users.noreply.github.com>
2026-04-10 08:11:33 -0400

    times for carousel

--- javascript/OurLab.js
+++ javascript/OurLab.js
@@ -21,7 +21,7 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 
-  setTimeout(showSlides, 2000);
+  setTimeout(showSlides, 4000);
 }
 // let slideIndex = 1;
 // showSlides(slideIndex);
